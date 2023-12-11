240a29fdd8abdaeb11dad7fdc4261aa12d388222
Antoliny0919 <antoliny0919@gmail.com>
2023-12-11 18:49:08 +0900

    userpage get data from server getServerSideProps section --> popularPost, latestPost cnt10

--- pages/[name]/index.jsx
+++ pages/[name]/index.jsx
@@ -5,11 +5,12 @@ import UserPageBody from '../../components/user/UserPageBody';
 // import CardSection from '../../components/common/card/CardSection';
 // import CardSwiper from '../../components/common/card/CardSwiper';
 
-export default function Index({ data, name }) {
+export default function Index({ name, posts }) {
+  console.log(posts);
   return (
     <>
       <UserPageHeader name={name} />
-      <UserPageBody data={data} />
+      <UserPageBody data={posts} />
       {/* <CardSwiper data={data}></CardSwiper>
       <PaddingBottom></PaddingBottom> */}
     </>
@@ -17,9 +18,23 @@ export default function Index({ data, name }) {
 }
 
 export const getServerSideProps = async (context) => {
+  // Section to be displayed on the user page data filtering fields
+  const queries = {
+    popularPost: 'ordering=-wave',
+    latestPost: 'ordering=-created_at',
+  };
   const { name } = context.query;
-  // get user posts
-  const response = await client.get(`http://api-local:8000/api/${name}/posts/`);
-  const data = await response.data;
-  return { props: { data, name } };
+
+  let posts = {};
+
+  for (const [section, query] of Object.entries(queries)) {
+    const response = await client.get(
+      `http://api-local:8000/api/${name}/posts/?${query}`,
+    );
+    const data = response.data;
+    // only get 10posts
+    posts = { ...posts, [section]: data.slice(0, 10) };
+  }
+
+  return { props: { name, posts } };
 };
